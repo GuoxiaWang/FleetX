58fba6f893e8df67379918912b5187fd14a5ce65
GuoxiaWang <mingzilaochongtu@gmail.com>
2023-03-02 17:07:34 +0800

    memory opt

--- ppfleetx/models/protein_folding/evoformer.py
+++ ppfleetx/models/protein_folding/evoformer.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import gc
 import paddle
 import paddle.nn as nn
 
@@ -214,7 +215,10 @@ class EvoformerIteration(nn.Layer):
         # [B, N_res//dap_size, N_res, c_z]
         residual = self.outer_product_mean(msa_act, msa_mask)
         outer_product_mean = self.outer_product_mean_dropout(residual)
-        pair_act = pair_act + outer_product_mean
+        #pair_act = pair_act + outer_product_mean
+        pair_act.add_(outer_product_mean)
+        del outer_product_mean
+        gc.collect()
 
         # [B, N_seq, N_res//dap_size, c_m] => [B, N_seq//dap_size, N_res, c_m]
         msa_act = dap.col_to_row(msa_act)
@@ -229,7 +233,10 @@ class EvoformerIteration(nn.Layer):
         residual = self.triangle_multiplication_outgoing(pair_act,
                                                          pair_mask_row)
         residual = self.triangle_outgoing_dropout(residual)
-        pair_act = pair_act + residual
+        #pair_act = pair_act + residual
+        pair_act.add_(residual)
+        del residual
+        gc.collect()
 
         # [B, N_res//dap_size, N_res, c_z] => [B, N_res, N_res//dap_size, c_z]
         pair_act = dap.row_to_col(pair_act)
@@ -237,7 +244,10 @@ class EvoformerIteration(nn.Layer):
         residual = self.triangle_multiplication_incoming(pair_act,
                                                          pair_mask_col)
         residual = self.triangle_incoming_dropout(residual)
-        pair_act = pair_act + residual
+        #pair_act = pair_act + residual
+        pair_act.add_(residual)
+        del residual
+        gc.collect()
 
         # [B, N_res, N_res//dap_size, c_z] => [B, N_res//dap_size, N_res, c_z]
         pair_act = dap.col_to_row(pair_act)
@@ -245,18 +255,27 @@ class EvoformerIteration(nn.Layer):
         residual = self.triangle_attention_starting_node(pair_act,
                                                          pair_mask_row)
         residual = self.triangle_starting_dropout(residual)
-        pair_act = pair_act + residual
+        #pair_act = pair_act + residual
+        pair_act.add_(residual)
+        del residual
+        gc.collect()
 
         # [B, N_res//dap_size, N_res, c_z] => [B, N_res, N_res//dap_size, c_z]
         pair_act = dap.row_to_col(pair_act)
         # [B, N_res, N_res//dap_size, c_z]
         residual = self.triangle_attention_ending_node(pair_act, pair_mask_col)
         residual = self.triangle_ending_dropout(residual)
-        pair_act = pair_act + residual
+        #pair_act = pair_act + residual
+        pair_act.add_(residual)
+        del residual
+        gc.collect()
 
         residual = self.pair_transition(pair_act, pair_mask)
         residual = self.pair_transition_dropout(residual)
-        pair_act = pair_act + residual
+        #pair_act = pair_act + residual
+        pair_act.add_(residual)
+        del residual
+        gc.collect()
 
         # [B, N_res, N_res//dap_size, c_z] => [B, N_res//dap_size, N_res, c_z]
         pair_act = dap.col_to_row(pair_act)
@@ -635,6 +654,9 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
         # preprocess_msa = self.preprocess_msa(batch['msa_feat'])
         msa_activations = paddle.unsqueeze(preprocess_1d, axis=1) + \
                     self.preprocess_msa(batch['msa_feat'])
+        del preprocess_1d
+        MB = 1024.0 * 1024.0
+        import gc
 
         right_single = self.right_single(
             batch['target_feat'])  # 1, n_res, 22 -> 1, n_res, 128
@@ -645,6 +667,12 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
         left_single = paddle.unsqueeze(
             left_single, axis=2)  # 1, n_res, 128 -> 1, n_res, 1, 128
         pair_activations = left_single + right_single
+        del left_single
+        del right_single
+        gc.collect()
+
+        # [B, N_res, N_res, c_z] => [B, N_res//dap_size, N_res, c_z]
+        pair_activations = dap.scatter(pair_activations, axis=1)
 
         mask_2d = paddle.unsqueeze(
             batch['seq_mask'], axis=1) * paddle.unsqueeze(
@@ -658,7 +686,11 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
                                                     batch['prev_pos'], None)
             dgram = dgram_from_positions(prev_pseudo_beta,
                                          **self.config.prev_pos)
+            dgram = dap.scatter(dgram, axis=1)
             pair_activations += self.prev_pos_linear(dgram)
+            del dgram
+            del prev_pseudo_beta
+            gc.collect()
 
         if self.config.recycle_features:
             if 'prev_msa_first_row' in batch:
@@ -672,9 +704,17 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
                 msa_first_row = paddle.unsqueeze(msa_first_row, axis=1)
                 msa_activations = paddle.concat(
                     [msa_first_row, msa_activations[:, 1:, :]], axis=1)
+                del prev_msa_first_row
+                del msa_first_row
+                gc.collect()
 
             if 'prev_pair' in batch:
-                pair_activations += self.prev_pair_norm(batch['prev_pair'])
+                prev_pair = batch['prev_pair']
+                prev_pair_gpu = prev_pair.cuda()
+                prev_pair_gpu = dap.scatter(prev_pair_gpu, axis=1)
+                pair_activations += self.prev_pair_norm(prev_pair_gpu)
+                del prev_pair_gpu
+                gc.collect()
 
         # RelPosEmbedder
         # Jumper et al. (2021) Suppl. Alg. 4 "relpos"
@@ -689,8 +729,12 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
                     min=0,
                     max=2 * self.config.max_relative_feature),
                 2 * self.config.max_relative_feature + 1)
+            rel_pos = dap.scatter(rel_pos, axis=1)
             rel_pos_bias = self.pair_activiations(rel_pos)
             pair_activations += rel_pos_bias
+            del rel_pos
+            del rel_pos_bias
+            gc.collect()
 
         # TemplateEmbedder
         # Jumper et al. (2021) Suppl. Alg. 2 "Inference" lines 9-13
@@ -707,27 +751,31 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
         # Jumper et al. (2021) Suppl. Alg. 2 "Inference" lines 14-16
         extra_msa_feat = self._create_extra_msa_feature(batch)
         extra_msa_activations = self.extra_msa_activations(extra_msa_feat)
+        del extra_msa_feat
+        gc.collect()
 
         # ==================================================
         #  Extra MSA Stack
         # Jumper et al. (2021) Suppl. Alg. 18 "ExtraMsaStack"
         # ==================================================
+
+        # scatter if using dap, otherwise do nothing
+        # [B, N_seq, N_res, c_m] => [B, N_seq//dap_size, N_res, c_m]
+        extra_msa_activations = dap.scatter(extra_msa_activations, axis=1)
+        # [B, N_seq, N_res, c_m] => [B, N_seq//dap_size, N_res, c_m]
+        msa_activations = dap.scatter(msa_activations, axis=1)
+
         extra_msa_stack_input = {
             'msa': extra_msa_activations,
             'pair': pair_activations,
         }
 
+        del pair_activations
+        gc.collect()
+
         if bp.get_world_size() > 1:
             extra_msa_stack_input['msa'] = bp.broadcast_grad_for_backward(
                 extra_msa_stack_input['msa'], 0)
-
-        # scatter if using dap, otherwise do nothing
-        # [B, N_seq, N_res, c_m] => [B, N_seq//dap_size, N_res, c_m]
-        extra_msa_stack_input['msa'] = dap.scatter(
-            extra_msa_stack_input['msa'], axis=1)
-        # [B, N_res, N_res, c_z] => [B, N_res//dap_size, N_res, c_z]
-        extra_msa_stack_input['pair'] = dap.scatter(
-            extra_msa_stack_input['pair'], axis=1)
 
         for idx, extra_msa_stack_iteration in enumerate(self.extra_msa_stack):
             extra_msa_act, extra_pair_act = recompute_wrapper(
@@ -747,11 +795,6 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
                 'pair': extra_msa_stack_output['pair']
             }
 
-        # gather if using dap, otherwise do nothing
-        # [B, N_res//dap_size, N_res, c_z] => [B, N_res, N_res, c_z]
-        extra_msa_stack_output['pair'] = dap.gather(
-            extra_msa_stack_output['pair'], axis=1)
-
         evoformer_input = {
             'msa': msa_activations,
             'pair': extra_msa_stack_output['pair'],
@@ -761,6 +804,10 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
             'msa': batch['msa_mask'],
             'pair': mask_2d,
         }
+
+        del extra_msa_stack_input
+        del extra_msa_stack_output
+        gc.collect()
 
         # ==================================================
         #  Template angle feat
@@ -814,12 +861,6 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
             evoformer_input['msa'] = bp.broadcast_grad_for_backward(
                 evoformer_input['msa'], 0)
 
-        # scatter if using dap, otherwise do nothing
-        # [B, N_seq, N_res, c_m] => [B, N_seq//dap_size, N_res, c_m]
-        evoformer_input['msa'] = dap.scatter(evoformer_input['msa'], axis=1)
-        # [B, N_res, N_res, c_z] => [B, N_res//dap_size, N_res, c_z]
-        evoformer_input['pair'] = dap.scatter(evoformer_input['pair'], axis=1)
-
         # ==================================================
         #  Main MSA Stack
         # Jumper et al. (2021) Suppl. Alg. 2 "Inference" lines 17-18
@@ -846,12 +887,15 @@ class DistEmbeddingsAndEvoformer(nn.Layer):
 
         msa_activations = evoformer_output['msa']
         pair_activations = evoformer_output['pair']
+        pair_activations_cpu = pair_activations.cpu()
+        del pair_activations
+        paddle.device.cuda.empty_cache()
         single_activations = self.single_activations(msa_activations[:, 0])
 
         num_seq = batch['msa_feat'].shape[1]
         output = {
             'single': single_activations,
-            'pair': pair_activations,
+            'pair': pair_activations_cpu,
             # Crop away template rows such that they are not used
             # in MaskedMsaHead.
             'msa': msa_activations[:, :num_seq],
